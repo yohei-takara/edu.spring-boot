179cefd74745e34b63d6bbb3fd2f145b2652b9c0
yohei-takara <yohei.takara@cacco.co.jp>
2017-09-21 11:37:37 +0900

    Mybatis サンプル実行

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,24 +2,47 @@ package com.example.demo.controller;
 
 import com.example.demo.model.CarManufacturers;
 import com.example.demo.repository.CarManufacturersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @EnableAutoConfiguration
+@RequestMapping("/")
 public class HelloController {
 
-    @Autowired
-    private CarManufacturersRepository car;
+    private final CarManufacturersRepository carManufacturersRepository;
 
-    @RequestMapping("/")
+    // Repository を インジェクションする
+    public HelloController(CarManufacturersRepository carManufacturersRepository) {
+        this.carManufacturersRepository = carManufacturersRepository;
+    }
+
+    @RequestMapping("/car")
+    @ResponseBody
+    public String car() {
+        List<CarManufacturers> carManufacturers = new ArrayList<>();
+        carManufacturers = carManufacturersRepository.fetchAll();
+
+
+        String result = "";
+        for (CarManufacturers carManufacturer : carManufacturers) {
+            result += carManufacturer.getName() + "<br>";
+        }
+
+        return result;
+    }
+
+
+
+    @RequestMapping("/hello")
     @ResponseBody
     public String home() {
-        CarManufacturers carManufacturers = car.fetch();
-        return "Hello " + carManufacturers.getName() ;
+        return "Hello World.";
     }
 
 }
--- src/main/java/com/example/demo/repository/CarManufacturersRepository.java
+++ src/main/java/com/example/demo/repository/CarManufacturersRepository.java
@@ -5,10 +5,16 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @Mapper
 public interface CarManufacturersRepository {
 
-    @Select("SELECT name from  car_manufacturers;")
-    CarManufacturers fetch();
+    @Select("SELECT name FROM  car_manufacturees")
+    List<CarManufacturers> fetchAll();
+
+
+    @Select("SELECT name FROM  car_manufacturees WHERE name = #{name};")
+    CarManufacturers fetch(String name);
 }
